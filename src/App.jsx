1f5842f03844305cb76ad299dafc7ae5d70ba453
ayushchandanworks <ayushchandanworks@gmail.com>
2024-01-31 11:58:46 +0000

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,47 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import './App.css';
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>This is the new app</h1>
-    </>
+    <div class="home">
+  <div class="components">
+    <div class="home2">
+      <div class="left-side">
+        <div class="ayush-chandan">Ayush Chandan</div>
+        <div class="software-engineer">Software Engineer</div>
+        <div class="button-div">
+          <div class="button">
+            <div class="learn-more">
+              <span>
+                <span class="learn-more-span">L</span>
+                <span class="learn-more-span2">earn More</span>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="frame-1">
+          <div class="website-still-in-development">
+            Website still in development
+          </div>
+          <img class="microsoft-computer-1" src="microsoft-computer-10.png" />
+        </div>
+      </div>
+      <div class="window">
+        <div class="bluebar">
+          <div class="icons">
+            <img class="bluebar-cross" src="bluebar-cross0.png" />
+            <img class="bluebar-maximize" src="bluebar-maximize0.png" />
+            <img class="bluebar-minimize" src="bluebar-minimize0.png" />
+          </div>
+        </div>
+        <img class="img-20230317173130-1" src="img-20230317173130-10.png" />
+      </div>
+    </div>
+  </div>
+</div>
+
   )
 }
 
