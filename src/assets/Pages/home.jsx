e6495b2b743d69b50cf0a1bb4c3987e140c42a11
ayushchandanworks <ayushchandanworks@gmail.com>
2024-02-01 16:01:26 +0000

    New Branch Develop

---
+++ src/assets/Pages/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <div>Navbar - Marquee</div>
+        <div>Components</div>
+        
+    </div>
+  )
+}
+
+export default Home
