42b2dd581698273c7d212430a9e44741b329e2cd
Ayush Chandan <0201csml001@niet.co.in>
2024-02-13 00:18:14 +0530

    valentine special

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './assets/Pages/Home.jsx';
 import './assets/CSS/App.css';
+import Indu from './assets/Pages/Indu.jsx';
 
 
 function App() {
 
   return (
-    <>
-      <Home />
-    </>
-
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='indu' element={<Indu />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
