a2d161bdcd196760ff60b1e0c716d9f34e3c1b0d
Ashkewww <ayushchandanworks@gmail.com>
2024-04-08 23:40:52 +0530

    Links fixes

--- src/componenets/Footer/Footer.tsx
+++ src/componenets/Footer/Footer.tsx
@@ -61,7 +61,7 @@ export default function Footer() {
                     <div className="w-full md:w-4/12 px-4 mx-auto text-center">
                         <div className="text-sm text-blueGray-500 font-semibold py-1">
                             Copyright © <span id="get-current-year">2024</span>
-                            <a href="https://www.creative-tim.com/product/notus-js" className="text-blueGray-500 hover:text-gray-800" target="_blank" > Ayush Chandan</a>
+                            &nbsp; Ayush Chandan
                         </div>
                     </div>
                 </div>
