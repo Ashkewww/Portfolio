c2844fdd6b3af2533d2024f88b0404d75886ecba
Ashkewww <ayushchandanworks@gmail.com>
2024-04-08 23:42:38 +0530

    weird apos fixed

--- src/componenets/Footer/Footer.tsx
+++ src/componenets/Footer/Footer.tsx
@@ -9,7 +9,7 @@ export default function Footer() {
             <div className="container mx-auto px-4">
                 <div className="flex flex-wrap text-left text-center">
                     <div className="w-full  px-4">
-                        <h4 className="text-3xl fonat-semibold text-blueGray-700">Let's keep in touch!</h4>
+                        <h4 className="text-3xl fonat-semibold text-blueGray-700">Let&apos;s keep in touch!</h4>
                         <h5 className="text-lg mt-0 mb-2 text-blueGray-600">
                             Find me on any of these platforms!
                         </h5>
